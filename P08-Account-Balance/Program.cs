1cd27cc7d47fef64a85a8a650e83e1a1fade3cff
DianaRangelova <diana_kirilova@mail.bg>
2024-02-11 22:51:31 +0200

    For Loop and While Loop

--- P08-Account-Balance/Program.cs
+++ P08-Account-Balance/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿double balance = 0;
+
+string input = Console.ReadLine();
+
+while (input != "End")
+{
+    double money = double.Parse(input);
+    if (money >= 0)
+    {
+        balance += money;
+        Console.WriteLine($"Increase: {money:F2}");
+    }
+    else
+    {
+        balance -= Math.Abs(money);
+        Console.WriteLine($"Decrease: {Math.Abs(money):F2}");
+    }
+
+
+    input = Console.ReadLine();
+}
+
+Console.WriteLine($"Balance: {balance:F2}");
